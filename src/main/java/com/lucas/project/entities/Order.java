9336343ed489f47e7f00d0f332901bfc5646327e
Lucas Sgalla <lucasgalla10@gmail.com>
2025-05-07 15:23:32 -0300

    Subtotal and total methods

--- src/main/java/com/lucas/project/entities/Order.java
+++ src/main/java/com/lucas/project/entities/Order.java
@@ -90,6 +90,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal(){
+        double sum = 0.0;
+        for (OrderItem x : items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
